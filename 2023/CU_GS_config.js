c7ede77ba7cb6d3ca799ab5e3c573bc570e5f215
Gavin Precurato <80174389+Gavo9649@users.noreply.github.com>
2023-03-13 15:01:06 -0400

    Update CU_GS_config.js

--- 2023/CU_GS_config.js
+++ 2023/CU_GS_config.js
@@ -17,7 +17,7 @@ var config_data = `
       "code": "e",
       "gsCol": "event",
       "type": "event",
-      "defaultValue": "2022carv",
+      "defaultValue": "MiaVal2023",
       "required": "true",
       "disabled": "true"
     },
